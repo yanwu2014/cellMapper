6e3b862a5ef5580f0d1d9efab86bdfff4234b7b4
Yan Wu <yauwning@gmail.com>
2018-05-21 19:22:54 -0700

    Add wrapper functions to streamline workflow

--- R/mapping.R
+++ R/mapping.R
@@ -2,58 +2,72 @@
 
 #' Run PCA
 #'
-#' @param norm.data Input data matrix, should be genes x samples
-#' @param genes.use Subset of genes to use
+#' @param x Input data matrix, should be genes x samples
 #' @param pcs.use Number of PCs to calculate
 #'
 #' @return List of PCA embeddings, loadings, and variance explained
 #' @import Matrix
 #' @import irlba
-#' @export
 #'
-FastPCA <- function(norm.data, genes.use, pcs.use = 40) {
-  genes.use <- intersect(genes.use, rownames(norm.data))
-  norm.data <- t(norm.data[genes.use,])
+fast_pca <- function(x, pcs.use) {
+  x <- Matrix::t(x)
 
-  cm <- Matrix::colMeans(norm.data)
-  pca.obj <- irlba::irlba(norm.data, nv = pcs.use, center = cm)
+  cm <- Matrix::colMeans(x)
+  pca.obj <- irlba::irlba(x, nv = pcs.use, center = cm)
 
   var.exp <- pca.obj$d
-  pc.emb <- t(pca.obj$u); colnames(pc.emb) <- rownames(norm.data);
+  pc.emb <- t(pca.obj$u); colnames(pc.emb) <- rownames(x);
   pc.load <- pca.obj$v; rownames(pc.load) <- genes.use;
   names(var.exp) <- rownames(pc.emb) <- colnames(pc.load) <- paste("PC", 1:pcs.use)
 
-  return(list(var.exp = var.exp, pc.load = pc.load, pc.emb = pc.emb))
+  return(list(pc.load = pc.load, pc.emb = pc.emb))
 }
 
 
 #' Projects new data onto existing PCs
 #'
-#' @param new.data Dataset to project, should be genes x samples
+#' @param newx Dataset to project, should be genes x samples
 #' @param pc.load Existing pca loadings
-#' @param genes.use Genes to use for projection
 #'
 #' @return Projected PCA embeddings
 #'
 #' @import Matrix
-#' @export
 #'
-ProjectPCA <- function(new.data, pc.load, genes.use) {
-  new.data <- t(new.data[genes.use,])
-  pc.load <- pc.load[genes.use,]
+project_pca <- function(newx, pc.load) {
+  if(!all(rownames(newx) == rownames(pc.load))) stop("New data must have same rownames")
+  newx <- Matrix::t(newx)
 
-  cm <- Matrix::colMeans(new.data)
-  pc.emb <- t((new.data - cm) %*% pc.load)
+  cm <- Matrix::colMeans(newx)
+  pc.emb <- t((newx - cm) %*% pc.load)
   return(pc.emb)
 }
 
 
+#' Runs PCA on the training dataset
+#'
+#' @param norm.counts Normalized gene expression matrix, should be genes x samples
+#' @param genes.use Genes to use for PCA
+#' @param ident Cell type identity
+#' @param pcs.use Number of PCs to calculate
+#'
+#' @return List of gene expression, cell identity, PCA embeddings, PCA loadings
+#'
+#' @import Matrix
+#' @import irlba
+#' @export
+#'
+TrainKNN <- function(norm.counts, genes.use, ident, pcs.use = 40) {
+  pca <- fast_pca(norm.counts[genes.use,], pcs.use = pcs.use)
+  list(norm.counts = norm.counts[genes.use,], ident = ident,
+       pc.emb = pca$pc.emb, pc.load = pca$pc.load)
+}
+
 
 #' Map cells to a reference dataset using a kNN classifier
 #'
-#' @param query.data Query matrix
-#' @param ref.data Reference matrix
-#' @param ref.ident Reference cell types
+#' @param query.norm.counts Query gene expression matrix
+#' @param train.knn Output of TrainKNN
+#' @param genes.use Genes to use for classification
 #' @param k Number of neighbors
 #'
 #' @return Matrix of cell type probabilities
@@ -62,67 +76,77 @@ ProjectPCA <- function(new.data, pc.load, genes.use) {
 #' @import compiler
 #' @export
 #'
-MapCellsKNN <- function(query.data, ref.data, ref.ident, k = 30) {
-  knn.res <- FNN::get.knnx(t(ref.data), t(query.data), k = k)
+MapKNN <- function(query.norm.counts, train.knn, genes.use = NULL, use.pca = T, k = 30) {
+  if (is.null(genes.use)) genes.use <- rownames(query.norm.counts)
 
-  ref.ident <- as.character(ref.ident)
-  unique.ident <- unique(ref.ident)
+  if (use.pca) {
+    genes.use <- intersect(genes.use, rownames(train.knn$pc.load))
+    query.pc.emb <- project_pca(query.norm.counts[genes.use,], train.knn$pc.load)
+    knn.res <- FNN::get.knnx(t(train.knn$pc.emb), t(query.pc.emb), k = k)
+  } else {
+    genes.use <- intersect(genes.use, rownames(train.knn$norm.counts))
+    knn.res <- FNN::get.knnx(t(as.matrix(train.knn$norm.counts[genes.use,])),
+                             t(as.matrix(query.norm.counts[genes.use,])), k = k)
+  }
 
-  cell.ident.frac <- apply(knn.res$nn.index, 1, function(x) {
+  ident <- as.character(train.knn$ident)
+  unique.ident <- unique(ident)
+
+  cell.mapped.ident <- apply(knn.res$nn.index, 1, function(x) {
     ident.counts <- vector(mode = "integer", length = length(unique.ident))
     names(ident.counts) <- unique.ident
 
-    ident.tbl <- table(ref.ident[x])
+    ident.tbl <- table(ident[x])
     ident.counts[names(ident.tbl)] <- ident.tbl
     ident.counts/k
   })
-  colnames(cell.ident.frac) <- colnames(query.data)
+  colnames(cell.mapped.ident) <- colnames(query.norm.counts)
 
-  return(cell.ident.frac)
+  return(cell.mapped.ident)
 }
 
-MapCellsKNN <- compiler::cmpfun(MapCellsKNN)
+MapKNN <- compiler::cmpfun(MapKNN)
 
 
 #' Summarize cell classification by group
 #'
-#' @param cell.ident.frac A reference groups x cells matrix of reference group probabilities
+#' @param cell.mapped.ident Mapped cell type probabilities
 #' @param query.ident Either a list or a factor of cell type identities for the query dataset
-#' @param ident.cell.thresh Filter out cells that don't map to a reference group with at least this probability
-#' @param hard.cell.thresh Assigns each cell to a single reference group
-#' @param min.ref.frac Filter out reference groups that are not mapped to with at least this probability
+#' @param ident.thresh Filter out cells that don't map to a reference identity
+#' @param assign.cells Assigns each cell to a single reference group
+#' @param min.ident.frac Filter out reference identities
 #'
 #' @return A reference groups x query groups matrix of probabilities
 #' @export
 #'
-SummarizeCellMapping <- function(cell.ident.frac, query.ident, ident.cell.thresh = 0, hard.cell.thresh = T,
-                                 min.ref.frac = 0.05) {
-  cell.ident.frac <- cell.ident.frac[,apply(cell.ident.frac, 2, max) > ident.cell.thresh]
+SummarizeKNN <- function(cell.mapped.ident, query.ident, ident.thresh = 0, assign.cells = T,
+                         min.ident.frac = 0.05) {
+  cell.mapped.ident <- cell.mapped.ident[,apply(cell.mapped.ident, 2, max) > ident.thresh]
 
-  if(hard.cell.thresh) {
-    cell.ident.frac <- apply(cell.ident.frac, 2, function(x) {
+  if(assign.cells) {
+    cell.mapped.ident <- apply(cell.mapped.ident, 2, function(x) {
       y <- rep(0, length(x)); names(y) <- names(x);
       y[which.max(x)] <- 1
       y
     })
   }
 
-  if (typeof(query.ident) == "list") {
-    query.ident <- lapply(query.ident, function(cells) intersect(cells, colnames(cell.ident.frac)))
-    query.ident.frac <- apply(cell.ident.frac, 1, function(x) {
+  if (is.list(query.ident)) {
+    query.ident <- lapply(query.ident, function(cells) intersect(cells, colnames(cell.mapped.ident)))
+    query.mean.ident <- apply(cell.mapped.ident, 1, function(x) {
       sapply(query.ident, function(cells) mean(x[cells]))
     })
   } else {
     query.ident <- factor(query.ident)
-    query.ident <- query.ident[colnames(cell.ident.frac)]
-    query.ident.frac <- apply(cell.ident.frac, 1, function(x) tapply(x, query.ident, mean))
+    query.ident <- query.ident[colnames(cell.mapped.ident)]
+    query.mean.ident <- apply(cell.mapped.ident, 1, function(x) tapply(x, query.ident, mean))
   }
 
-  query.ident.frac <- t(query.ident.frac)
-  return(query.ident.frac[apply(query.ident.frac, 1, max) >= min.ref.frac,])
+  query.mean.ident <- t(query.mean.ident)
+  return(query.mean.ident[apply(query.mean.ident, 1, max) >= min.ident.frac,])
 }
 
-SummarizeCellMapping <- compiler::cmpfun(SummarizeCellMapping)
+SummarizeKNN <- compiler::cmpfun(SummarizeKNN)
 
 
 #' Map mouse genes to human genes
